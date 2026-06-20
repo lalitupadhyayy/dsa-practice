929041703317c9c0a7b46c30c8bd8dcb45147ea6
Lalit Kumar Upadhyay <lalitupadhyay522@gmail.com>
2026-06-20 22:23:17 +0530

    solve problem Longest_Substring_with_Same_Letters_after_Replacement.cpp

---
+++ sliding window/Longest_Substring_with_Same_Letters_after_Replacement.cpp
@@ -0,0 +1,43 @@
+// Problem: Longest Substring with Same Letters after Replacement (hard)
+// Leet code: 424
+// problem level : medium
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+       int n=s.size();
+       vector<int> f(256,0);
+       int low=0,high=0,res=INT_MIN;
+       for(high=0;high<n;high++)
+       {
+           f[s[high]]++;
+           int maxcnt=find(f);
+           int len=high-low+1;
+           int diff=len-maxcnt;
+           while(diff>k)
+           {
+               f[s[low]]--;
+               low++;
+               maxcnt=find(f);
+               len=high-low+1;
+               diff=len-maxcnt;
+           }
+           len=high-low+1;
+           res=max(res,len);
+       }
+       return res;
+
+    }
+    
+    int find(vector<int> &a)
+    {
+        int maxc=-1;
+        for(int i=0;i<256;i++)
+        maxc=max(maxc,a[i]);
+        return maxc;
+    }
+
+};
