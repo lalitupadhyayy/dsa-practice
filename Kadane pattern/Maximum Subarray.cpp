185623fa030f949333285ea1ba1db6fa7ae9688e
Lalit Kumar Upadhyay <lalitupadhyay522@gmail.com>
2026-08-20 23:21:10 +0530

    Add Maximum Subarray solution using Kadane's Algorithm
    Implement Kadane's algorithm to find the maximum subarray sum.

---
+++ Kadane pattern/Maximum Subarray.cpp
@@ -0,0 +1,24 @@
+// Problem: Maximum Subarray
+// LeetCode: 53
+// Pattern: Kadane's Algorithm
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            currentSum = max(nums[i], currentSum + nums[i]);
+
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
