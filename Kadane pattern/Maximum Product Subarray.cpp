a0182d20723ad410aeeaccefaf43cf86b70b7dd9
Lalit Kumar Upadhyay <lalitupadhyay522@gmail.com>
2026-08-25 17:00:47 +0530

    Refactor maxProduct to use separate max and min ends

--- Kadane pattern/Maximum Product Subarray.cpp
+++ Kadane pattern/Maximum Product Subarray.cpp
@@ -9,21 +9,21 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
 
-        int currMax = nums[0];
-        int currMin = nums[0];
+        int maxEnd = nums[0];
+        int minEnd = nums[0];
         int ans = nums[0];
 
         for (int i = 1; i < nums.size(); i++) {
 
-            // Negative number can turn min into max
-            if (nums[i] < 0) {
-                swap(currMax, currMin);
-            }
+            int v1 = nums[i];
+            int v2 = maxEnd * nums[i];
+            int v3 = minEnd * nums[i];
 
-            currMax = max(nums[i], currMax * nums[i]);
-            currMin = min(nums[i], currMin * nums[i]);
+            maxEnd = max(v1, max(v2,v3));
+            minEnd = min(v1, min(v2,v3));
 
-            ans = max(ans, currMax);
+            ans = max(ans, max(maxEnd,minEnd));
+            
         }
 
         return ans;
